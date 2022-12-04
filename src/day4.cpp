412ebc1dc2dd4d168b92df4cc8dce7ea22e99c3f
MinusKube <minuskube@gmail.com>
2022-12-04 16:56:13 +0100

    Clean day 4 solution

--- src/day4.cpp
+++ src/day4.cpp
@@ -3,70 +3,66 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <xstring>
 
 #include "day.h"
 
-struct Pair {
-	std::vector<int> elements;
-	
-	Pair(int a, int b) {
-		for (int i = a; i <= b; i++) {
-			elements.push_back(i);
-		}
+class Range {
+	int m_start;
+	int m_end;
+
+public:
+	Range() {}
+	Range(int start, int end) : m_start(start), m_end(end) {}
+
+	bool contains(int value) {
+		return value >= m_start && value <= m_end;
+	}
+
+	bool contains(Range range) {
+		return range.m_start >= m_start && range.m_end <= m_end;
+	}
+
+	bool intersects(Range range) {
+		return range.m_start <= m_end && range.m_end >= m_start;
+	}
+
+	friend std::istream& operator>> (std::istream& stream, Range& range) {
+		stream >> range.m_start;
+		stream.ignore();
+		stream >> range.m_end;
+
+		return stream;
 	}
 };
 
 static void run(std::ifstream& file) {
-	int t = 0;
-	int t2 = 0;
+	int contained_pairs = 0;
+	int intersecting_pairs = 0;
+
 	std::string line;
 	while (std::getline(file, line)) {
 		std::istringstream stream(line);
 
-		int a, b, c, d;
-		char temp;
-		stream >> a >> temp >> b >> temp >> c >> temp >> d;
+		Range rangeA;
+		stream >> rangeA;
 
-		Pair pairA(a, b);
-		Pair pairB(c, d);
+		stream.ignore();
 
-		bool nop = true;
-		for (int e : pairA.elements) {
-			if (std::find(pairB.elements.begin(), pairB.elements.end(), e) == pairB.elements.end()) {
-				nop = false;
-			}
+		Range rangeB;
+		stream >> rangeB;
+
+		if (rangeA.contains(rangeB) || rangeB.contains(rangeA)) {
+			contained_pairs += 1;
 		}
 
-		if (nop) {
-			t += 1;
-		}
-		else {
-		nop = true;
-		for (int e : pairB.elements) {
-			if (std::find(pairA.elements.begin(), pairA.elements.end(), e) == pairA.elements.end()) {
-				nop = false;
-			}
-		}
-
-		if (nop) {
-			t += 1;
-		}
-		}
-
-		bool found = false;
-		for (int e : pairA.elements) {
-			if (std::find(pairB.elements.begin(), pairB.elements.end(), e) != pairB.elements.end()) {
-				found = true;
-			}
-		}
-
-		if (found) {
-			t2 += 1;
+		if (rangeA.intersects(rangeB)) {
+			intersecting_pairs += 1;
 		}
 	}
 
-	print_p1_result(t);
-	print_p2_result(t2);
+	print_p1_result(contained_pairs);
+	print_p2_result(intersecting_pairs);
 }
 
 static Day day(4, run);
