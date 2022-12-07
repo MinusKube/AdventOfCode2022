abe16942ef132ef6149f14e0bcd9b1c7f8aabcf7
MinusKube <minuskube@gmail.com>
2022-12-07 19:39:33 +0100

    Clean solution for day 7

--- src/day7.cpp
+++ src/day7.cpp
@@ -1,21 +1,22 @@
 #include <algorithm>
-#include <functional>
 #include <iostream>
+#include <limits>
 #include <sstream>
-#include <deque>
 #include <stdexcept>
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
 
 #include "day.h"
 
 struct Content {
 	bool dir;
 	int size;
-	std::string name;
+	std::string full_name;
 };
 
-static int get_folder_size(const std::unordered_map<std::string, std::vector<Content>>& folder_contents, std::string folder) {
+typedef std::unordered_map<std::string, std::vector<Content>> FolderContents;
+
+static int get_folder_size(const FolderContents& folder_contents, const std::string& folder) {
 	int size = 0;
 
 	if (folder_contents.find(folder) == folder_contents.end()) {
@@ -24,7 +25,7 @@ static int get_folder_size(const std::unordered_map<std::string, std::vector<Con
 
 	for (Content content : folder_contents.at(folder)) {
 		if (content.dir) {
-			size += get_folder_size(folder_contents, content.name);
+			size += get_folder_size(folder_contents, content.full_name);
 		}
 		else {
 			size += content.size;
@@ -35,11 +36,7 @@ static int get_folder_size(const std::unordered_map<std::string, std::vector<Con
 }
 
 static void run(std::ifstream& file) {
-	int result_p1 = 0;
-	int result_p2 = 0;
-
-	std::unordered_map<std::string, std::vector<Content>> folder_contents;
-
+	FolderContents folder_contents;
 	std::vector<std::string> current_dir;
 
 	std::string line;
@@ -49,10 +46,10 @@ static void run(std::ifstream& file) {
 		if (stream.peek() == '$') {
 			stream.ignore();
 
-			std::string cmd;
-			stream >> cmd;
+			std::string command;
+			stream >> command;
 
-			if (cmd == "cd") {
+			if (command == "cd") {
 				std::string path;
 				stream >> path;
 
@@ -68,65 +65,61 @@ static void run(std::ifstream& file) {
 			}
 		}
 		else {
-			std::string cur = "/";
+			std::string current_path = "/";
 			for (auto dir : current_dir) {
-				cur += dir;
-				cur += "/";
+				current_path += dir + "/";
 			}
 
 			std::string type;
 			stream >> type;
 
+			std::string name;
+			stream >> name;
+
+			Content content;
+
 			if (type == "dir") {
-				Content content;
 				content.dir = true;
-				std::string name;
-				stream >> name;
-				content.name = cur + name + "/";
-
-				if (folder_contents.find(cur) == folder_contents.end()) {
-					folder_contents.emplace(cur, std::vector<Content>());
-				}
-
-				folder_contents[cur].push_back(content);
+				content.size = 0;
+				content.full_name = current_path + name + "/";
 			}
 			else {
-				Content content;
 				content.dir = false;
 				content.size = std::stoi(type);
-				stream >> content.name;
-
-				if (folder_contents.find(cur) == folder_contents.end()) {
-					folder_contents.emplace(cur, std::vector<Content>());
-				}
-
-				folder_contents[cur].push_back(content);
+				content.full_name = current_path + name;
 			}
+
+			folder_contents[current_path].push_back(content);
 		}
 	}
 
-	int total_size = 70000000;
-	int used_size = get_folder_size(folder_contents, "/");
-	int needed_size = 30000000;
-	std::vector<int> ok;
-	for (auto iter : folder_contents) {
-		std::string folder = iter.first;
-		int size = get_folder_size(folder_contents, folder);
+	const int maximum_folder_size = 100000;
+	const int total_size = 70000000;
+	const int used_size = get_folder_size(folder_contents, "/");
+	const int needed_size = 30000000;
 
-		if (size <= 100000) {
-			result_p1 += size;
+	int folder_size_sum = 0;
+	int min_removable_size = std::numeric_limits<int>::max();
+
+	for (auto& pair : folder_contents) {
+		const std::string& folder = pair.first;
+		const int size = get_folder_size(folder_contents, folder);
+
+		// Part One
+		if (size <= maximum_folder_size) {
+			folder_size_sum += size;
 		}
 
-		if (total_size - used_size + size >= needed_size) {
-			ok.push_back(size);
+		// Part Two
+		if (total_size - used_size + size >= needed_size &&
+			size < min_removable_size) {
+
+			min_removable_size = size;
 		}
 	}
 
-	std::sort(ok.begin(), ok.end());
-	result_p2 = ok[0];
-
-	print_p1_result(result_p1);
-	print_p2_result(result_p2);
+	print_p1_result(folder_size_sum);
+	print_p2_result(min_removable_size);
 }
 
 static Day day(7, run);
