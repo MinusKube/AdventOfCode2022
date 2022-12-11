965cfe36e4c84783ebc6c53d9f6a2ff061f01437
MinusKube <minuskube@gmail.com>
2022-12-11 01:12:17 +0100

    Clean solution for day 10

--- src/day10.cpp
+++ src/day10.cpp
@@ -1,38 +1,34 @@
 #include <algorithm>
-#include <array>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
-#include <unordered_set>
 
 #include "day.h"
 
-static void increase_cycle(int& cycle, int& x, int& p1, std::string& p2) {
-	cycle += 1;
-
-	if (cycle == 20 || (cycle - 20) % 40 == 0) {
-		p1 += cycle * x;
-	}
-
-	if (std::abs((x + 1) - (cycle % 40)) <= 1) {
-		p2 += "#";
-	}
-	else {
-		p2 += ".";
-	}
-
-	if (cycle % 40 == 0) {
-		p2 += "\n";
-	}
-}
-
 static void run(std::ifstream& file) {
-	int p1 = 0;
-	std::string p2 = "\n";
+	int total_signal_strength = 0;
+	std::string screen = "\n";
 
 	int cycle = 0;
-	int x = 1;
+	int register_value = 1;
+
+	auto increase_cycle = [&] () {
+		cycle += 1;
+
+		if (cycle == 20 || (cycle - 20) % 40 == 0) {
+			total_signal_strength += cycle * register_value;
+		}
+
+		int sprite_center_column = register_value + 1;
+		int pixel_column = cycle % 40;
+
+		screen += std::abs(sprite_center_column - pixel_column) <= 1 ? '#' : '.';
+		
+		if (pixel_column == 0) {
+			screen += "\n";
+		}
+	};
 
 	std::string line;
 	while (std::getline(file, line)) {
@@ -42,21 +38,21 @@ static void run(std::ifstream& file) {
 		stream >> operation;
 
 		if (operation == "noop") {
-			increase_cycle(cycle, x, p1, p2);
+			increase_cycle();
 		}
 		else if (operation == "addx") {
-			increase_cycle(cycle, x, p1, p2);
-			increase_cycle(cycle, x, p1, p2);
+			increase_cycle();
+			increase_cycle();
 
 			int value;
 			stream >> value;
 
-			x += value;
+			register_value += value;
 		}
 	}
 
-	print_p1_result(p1);
-	print_p2_result(p2);
+	print_p1_result(total_signal_strength);
+	print_p2_result(screen);
 }
 
 static Day day(10, run);
